899e2f72fb396f31accbd8185673a95b28b26f4c
Antoine Martin <toinux@gmail.com>
2023-12-08 21:43:16 +0100

    docker : add AUTOUPDATE option and handle corrupted downloaded anothertime.jar

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,11 @@
 FROM eclipse-temurin:21-jdk-alpine
+
 ENV TZ="Europe/Paris"
+ENV AUTOUPDATE=true
 ENV ANOTHERTIME_AWTRIX_TOPIC=awtrix_xxxxxx
 ENV ANOTHERTIME_AWTRIX_URL=http://awtrix_ip_address
 ENV ANOTHERTIME_BROKER_URL=tcp://mqtt_address:1883
+
 RUN apk --no-cache add py3-pip
 RUN pip install lastversion
 
--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -1,18 +1,30 @@
 #!/usr/bin/env bash
 
+set -o pipefail
+
+AUTOUPDATE=${AUTOUPDATE:-true}
+
 CURRENT_VERSION=0
+GITHUB_REPO="toinux/anothertime"
+GITHUB_URL="https://github.com/${GITHUB_REPO}"
+
 
 if [[ -f anothertime.jar ]]; then
-  CURRENT_VERSION=$(unzip -q -p anothertime.jar META-INF/MANIFEST.MF | awk -F' ' '/Implementation-Version/ {print $2}')
+  if ! CURRENT_VERSION=$(unzip -q -p anothertime.jar META-INF/MANIFEST.MF | awk -F' ' '/Implementation-Version/ {print $2}'); then
+    CURRENT_VERSION=0
+  fi
 fi
 
-LAST_VERSION=$(lastversion --at github toinux/anothertime -gt "${CURRENT_VERSION}")
-
-if [[ $? -eq 0 ]]; then
-  echo "Updating anothertime to $LAST_VERSION"
-  lastversion --assets --at github toinux/anothertime -gt "${CURRENT_VERSION}" -o anothertime.jar
-else
-  echo "Anothertime already to latest version : ${CURRENT_VERSION}"
+if [[ "${CURRENT_VERSION}" = 0 ]] ; then
+  echo "anothertime.jar not found, fetching last version on ${GITHUB_URL}"
+  lastversion --assets --at github "${GITHUB_REPO}" -gt "${CURRENT_VERSION}" -o anothertime.jar
+elif [[ "${AUTOUPDATE}" = true ]] ; then
+  echo "AUTOUPDATE=true, checking last version on ${GITHUB_URL}"
+  if LAST_VERSION=$(lastversion --at github "${GITHUB_REPO}" -gt "${CURRENT_VERSION}"); then
+    echo "New version found, updating to $LAST_VERSION"
+    lastversion --assets --at github "${GITHUB_REPO}" -gt "${CURRENT_VERSION}" -o anothertime.jar
+  else
+    echo "anothertime.jar already to latest version : ${CURRENT_VERSION}"
+  fi
 fi
-
 java -jar anothertime.jar
